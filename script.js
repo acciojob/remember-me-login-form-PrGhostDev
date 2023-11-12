e031ee8766cc416088c5d43ac30fd051140a7691
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-11-12 20:21:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if there are saved details
+    if (localStorage.getItem('rememberedUser')) {
+        // Show the "Login as existing user" button
+        var existingUserButton = document.createElement('button');
+        existingUserButton.setAttribute('id', 'existing');
+        existingUserButton.textContent = 'Login as existing user';
+        existingUserButton.addEventListener('click', function () {
+            // Retrieve and display the saved username
+            var savedUsername = localStorage.getItem('rememberedUser');
+            alert('Logged in as ' + savedUsername);
+        });
+
+        document.body.appendChild(existingUserButton);
+    }
+
+    // Add event listener to the login form
+    document.getElementById('loginForm').addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        var rememberCheckbox = document.getElementById('checkbox');
+
+        // Save details to local storage if the checkbox is checked
+        if (rememberCheckbox.checked) {
+            localStorage.setItem('rememberedUser', username);
+            localStorage.setItem('rememberedPassword', password);
+        } else {
+            // Remove stored details if the checkbox is not checked
+            localStorage.removeItem('rememberedUser');
+            localStorage.removeItem('rememberedPassword');
+        }
+
+        // Show the login alert
+        alert('Logged in as ' + username);
+    });
+});//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if there are saved details
+    if (localStorage.getItem('rememberedUser')) {
+        // Show the "Login as existing user" button
+        var existingUserButton = document.createElement('button');
+        existingUserButton.setAttribute('id', 'existing');
+        existingUserButton.textContent = 'Login as existing user';
+        existingUserButton.addEventListener('click', function () {
+            // Retrieve and display the saved username
+            var savedUsername = localStorage.getItem('rememberedUser');
+            alert('Logged in as ' + savedUsername);
+        });
+
+        document.body.appendChild(existingUserButton);
+    }
+
+    // Add event listener to the login form
+    document.getElementById('loginForm').addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        var rememberCheckbox = document.getElementById('checkbox');
+
+        // Save details to local storage if the checkbox is checked
+        if (rememberCheckbox.checked) {
+            localStorage.setItem('rememberedUser', username);
+            localStorage.setItem('rememberedPassword', password);
+        } else {
+            // Remove stored details if the checkbox is not checked
+            localStorage.removeItem('rememberedUser');
+            localStorage.removeItem('rememberedPassword');
+        }
+
+        // Show the login alert
+        alert('Logged in as ' + username);
+    });
+});
